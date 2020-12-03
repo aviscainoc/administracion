4f47e2a35e89a4b8020510340da3c118d02f9aff
unknown <marcelo_1993@hotmail.es>
2020-12-03 14:30:59 -0500

    avance

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+import mysql.connector as mcdb
 
+#aqui creamos la conexion con la base
+conn =mcdb.connect(host="localhost", user="root", passwd="cuenca", database="promoscuencav2")
+
+print('conexion exitosa a la base promoshop')
+cur=conn.cursor()
+
+
+# recordar siempre debo instalar  pip install mysql-connector
 html_base = """
     <h1>Mi empresa</h1>
     <ul>
@@ -7,6 +16,7 @@ html_base = """
         <li><a href="/empresas/">Empresas</a></li>
         <li><a href="/productoServicios">Productos y Servicios</a></li>
         <li><a href="/reportes/">Reportes</a></li>
+        <li><a href="/login/">Iniciar Sesion</a></li>
     </ul>
 """
 
@@ -14,10 +24,20 @@ def home(request):
     return render(request,"core/home.html")
 
 def empresas(request):
-    return render(request,"core/empresas.html")
+    cur.execute("select co.id, co.nombre, co.fechaCreacion from empresa as co ")
+    datos= cur.fetchall()    
+    print(list(datos) ,'\n')    
+    return render(request,'core/empresas.html',{'empresa':datos})
+
 
 def productoServicios(request):
     return render(request,"core/productoServicios.html")
 
 def reportes(request):
     return render(request,"core/reportes.html")
+
+def login(request):
+    return render(request,"core/login.html")
+
+
+#metodo para leer la base y hacer consultas
